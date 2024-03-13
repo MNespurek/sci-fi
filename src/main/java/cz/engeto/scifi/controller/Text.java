9dc2ce9a4c8b6639f32d3a543a7fd41f70c359b9
michal.nespurek <michal.nespurek@email.cz>
2024-03-13 10:40:55 +0100

    Vytvoření vlastní třídy pro výjimky TextException Úprava třídy Text - vytvoření 3 endpointů dle zadání.

--- src/main/java/cz/engeto/scifi/controller/Text.java
+++ src/main/java/cz/engeto/scifi/controller/Text.java
@@ -1,6 +1,71 @@
 package cz.engeto.scifi.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+@RestController
 public class Text {
+    @GetMapping("romantic")
+    public StringBuilder loadFromFileRomantic(String fileName) throws TextException{
+        fileName = Settings.getRomantic();
+        StringBuilder romantic = new StringBuilder();
+        try (Scanner scanner = new Scanner(new BufferedReader(new FileReader(fileName)))) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] parts = line.split(" ");
+
+                for(String part : parts) {
+                    romantic.append(part).append(" ");
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Error while reading: " + e.getLocalizedMessage());
+        }
+        return romantic;
+    }
+
+    @GetMapping("historic")
+    public StringBuilder loadFromFileHistoric(String fileName) throws TextException{
+        fileName = Settings.getHistoric();
+        StringBuilder historic = new StringBuilder();
+        try (Scanner scanner = new Scanner(new BufferedReader(new FileReader(fileName)))) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] parts = line.split(" ");
+
+                for(String part : parts) {
+                    historic.append(part).append(" ");
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Error while reading: " + e.getLocalizedMessage());
+        }
+        return historic;
+    }
+
+    @GetMapping("scifi")
+    public StringBuilder loadFromFileScifi(String fileName) throws TextException{
+        fileName = Settings.getHistoric();
+        StringBuilder scifi = new StringBuilder();
+        try (Scanner scanner = new Scanner(new BufferedReader(new FileReader(fileName)))) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] parts = line.split(" ");
+
+                for(String part : parts) {
+                    scifi.append(part).append(" ");
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Error while reading: " + e.getLocalizedMessage());
+        }
+        return scifi;
+    }
 
 
 
---
+++ src/main/java/cz/engeto/scifi/controller/TextException.java
@@ -0,0 +1,7 @@
+package cz.engeto.scifi.controller;
+
+public class TextException extends Exception {
+    TextException(String message) {
+        super(message);
+    }
+}
